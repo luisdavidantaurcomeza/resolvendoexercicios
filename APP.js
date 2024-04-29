fac8b7c4bd20e304f89c999f3c1bbf9048f9823d
luis david <luisdavid_204@hotmail.com>
2024-04-29 16:48:10 -0300

    iniciando os aplicativos principais, app.js e o index.html e resolver os exercicios propostos

---
+++ APP.js
@@ -0,0 +1,78 @@
+//Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+
+//let mensagem = 'Boas vindas ao nosso site: ';
+//alert (mensagem);
+
+// Declare uma variável chamada nome e atribua a ela o valor "Lua".
+
+//let nome = 'lua';
+
+//Crie uma variável chamada idade e atribua a ela o valor 25:
+
+//let idade = 25;
+
+//Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+
+//let numeroDeVendas = 50;
+
+//Defina uma variável saldoDisponivel e atribua a ela o valor 1000. 
+
+//let saldoDisponivel = 1000; 
+
+//Exiba um alerta com o texto "Erro! Preencha todos os campos";
+
+/* let mensagem = 'Erro! Preencha todos os campos';
+alert (mensagem);  */
+
+//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+/* let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert (mensagemDeErro); */
+
+//Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+
+/* let nome = prompt('Digite o seu nome');
+ */
+
+//peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+/* let idade = prompt ('informe sua idade: ');  */
+
+//Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+
+/* let idade = prompt ('informe sua idade: ');
+
+if (idade>=18){
+    alert('pode tirar sua habilitação');
+} else {
+    alert('ainda não é possível tirar a habilitação')
+}
+ */
+
+//Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+/* let diaDaSemana = prompt ('Qual o dia da semana? segunda, terça, quarta, quinta, sexta, sábado ou domingo');
+
+switch (diaDaSemana) {
+    case 'segunda'||'SEGUNDA'||'Segunda'||'SEgunda'||'SEGunda'||'SEGUnda'||'SEGUNda':
+      alert ('Tenha uma boa semana! ');
+        break;
+    case 'terça'||'Terça'||'TErça'||'TERça'||'TERÇa'||'TERÇA':
+      alert ('Tenha uma boa semana! ');
+        break;
+    case 'quarta'||'Quarta'||'QUarta'||'QUArta'||'QUARta'||'QUARTa'||'QUARTA':
+      alert ('Tenha uma boa semana! ');
+        break;
+    case 'quinta'||'Quinta'||'QUinta'||'QUInta'||'QUINta'||'QUINTa'||'QUINTA':
+      alert ('Tenha uma boa semana! ');  
+        break;
+    case 'sexta'||'Sexta'||'SExta'||'SEXta'||'SEXTa'||'SEXTA':
+      alert ('Tenha uma bom fim de semana! ');
+        break;
+    case 'sábado'||'Sábado'||'SÁbado'||'SÁBado'||'SÁBAdo'||'SÁBADo'||'SÁBADO':
+      alert ('Tenha um bom fim de semana! ') 
+        break;  
+    case 'domingo'||'Domingo'||'DOmingo'||'DOMingo'||'DOMIngo'||'DOMINgo'||'DOMINGo'||'DOMINGO':
+      alert ('Tenha um bom fim de semana! ')     
+      break;
+    default:
+      alert ('digite um dia válido! ')
+      break;
+  }  */
